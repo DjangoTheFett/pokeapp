845dccb631b2b302f1fd60b2c6feb5c8325913e6
Jacob Cash <147017234+DjangoTheFett@users.noreply.github.com>
2026-02-05 06:03:00 -0600

    added pulling of hp stat to confirm how this functions , added some styling

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const container = document.getElementById("container")
 const imgContainer = document.getElementById("img")
 const textField = document.getElementById("pokemon")
 const pokemonName = document.getElementById("name")
+const pokeHp = document.getElementById("poke-hp")
+const pokeStats = document.getElementById("poke-stats")
+const pokeDiv = document.getElementById("poke-div")
 
 
 
@@ -17,6 +20,10 @@ searchButton.addEventListener("click", async function(event) {
              
         const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonChoice}`)
 
+         if (!apiResponse.ok) {
+            throw new Error("error thrown")
+        }
+
         const data =  await apiResponse.json();
 
         
@@ -25,6 +32,12 @@ searchButton.addEventListener("click", async function(event) {
 
         pokemonName.innerHTML = data.name
         imgContainer.src  = data.sprites.front_default
+        pokeHp.innerHTML = `HP: ${data.stats[0].base_stat}`
+        pokeStats.style.visibility = "visible"
+        pokeDiv.style.visibility = "visible"
+
+       
+        
 
         
 
@@ -32,7 +45,12 @@ searchButton.addEventListener("click", async function(event) {
 
     } catch (error) {
 
-        console.error(error)
+        console.error(error.message)
+        pokemonName.innerHTML = "ERROR: you seem to of typed something thats not a pokemon name, please try again."
+        pokeDiv.style.visibility = "visible"
+        pokeStats.style.visibility = "hidden"
+        imgContainer.style.visibility = "hidden"
+        
     }
 
     
