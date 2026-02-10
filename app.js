e99042576970df6ab3aeae474340b5b56ae79f59
Jacob Cash <147017234+DjangoTheFett@users.noreply.github.com>
2026-02-09 20:17:44 -0600

    added pages for compare and battle and began editing the compare page. refactored the render function.

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const container = document.getElementById("container")
 const imgContainer = document.getElementById("img")
 const textField = document.getElementById("pokemon")
 const pokemonName = document.getElementById("name")
-const pokeHp = document.getElementById("poke-hp")
-const pokeAtt = document.getElementById("poke-att")
-const pokeDef = document.getElementById("poke-def")
-const pokeSpecAtt = document.getElementById("poke-specatt")
-const pokeSpecDef = document.getElementById("poke-specdef")
-const pokeSpeed = document.getElementById("poke-speed")
 const pokeStats = document.getElementById("poke-stats")
 const pokeDiv = document.getElementById("poke-div")
 
@@ -18,16 +12,25 @@ const pokeDiv = document.getElementById("poke-div")
 
 let renderPokemon = function(data) {
 
+    const statData = data.stats;
+
      pokemonName.innerHTML = data.name
-        imgContainer.src  = data.sprites.front_default
-        pokeHp.innerHTML = `HP: ${data.stats[0].base_stat}`
-        pokeAtt.innerHTML = `ATT: ${data.stats[1].base_stat}`
-        pokeDef.innerHTML = `DEF: ${data.stats[2].base_stat}`
-        pokeSpecAtt.innerHTML = `SPEC ATT: ${data.stats[3].base_stat}`
-        pokeSpecDef.innerHTML = `SPEC DEF: ${data.stats[4].base_stat}`
-        pokeSpeed.innerHTML = `SPEED: ${data.stats[5].base_stat}`
-        pokeStats.style.visibility = "visible"
-        pokeDiv.style.visibility = "visible"
+        
+        pokeStats.innerHTML = "";
+        for (let i = 0; i < statData.length; i++) {
+            const paragraph = document.createElement("p")
+
+            paragraph.textContent = `${statData[i].stat.name}: ${statData[i].base_stat} `
+
+            pokeStats.appendChild(paragraph)
+            
+        }
+         imgContainer.src  = data.sprites.front_default
+          pokeStats.style.visibility = "visible"
+         pokeDiv.style.visibility = "visible"
+          imgContainer.style.visibility = "visible"
+
+        
 }
 
 
