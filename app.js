0fa6e40820739007a6628e960fc6eb7be8c4be3b
Jacob Cash <147017234+DjangoTheFett@users.noreply.github.com>
2026-02-15 18:47:34 -0600

    refactored api call and rendering of components made changes to compare page styling

--- app.js
+++ app.js
@@ -6,54 +6,47 @@ const textField = document.getElementById("pokemon")
 const pokemonName = document.getElementById("name")
 const pokeStats = document.getElementById("poke-stats")
 const pokeDiv = document.getElementById("poke-div")
+const compBtnOne = document.getElementById("comp-btn-one")
+const compDivOne = document.getElementById("pokemon-1")
+const compStatsOne = document.getElementById("stats-1")
+const compImgOne = document.getElementById("poke-img-one")
+const compTextOne = document.getElementById("comp-text-one")
+const compBtntwo = document.getElementById("comp-btn-two")
+const compDivtwo = document.getElementById("pokemon-2")
+const compStatstwo = document.getElementById("stats-2")
+const compImgtwo = document.getElementById("poke-img-two")
+const compTexttwo = document.getElementById("comp-text-two")
+const compNameTwo = document.getElementById("comp-name-two")
 
 
 
+//--------------------------------------------------------------------------------------------------------
 
-let renderPokemon = function(data) {
+const apiCall = async function(text,stats,pContainer,pImg,nameContainer){
 
-    const statData = data.stats;
-
-     pokemonName.innerHTML = data.name
-        
-        pokeStats.innerHTML = "";
-        for (let i = 0; i < statData.length; i++) {
-            const paragraph = document.createElement("p")
-
-            paragraph.textContent = `${statData[i].stat.name}: ${statData[i].base_stat} `
-
-            pokeStats.appendChild(paragraph)
-            
-        }
-         imgContainer.src  = data.sprites.front_default
-          pokeStats.style.visibility = "visible"
-         pokeDiv.style.visibility = "visible"
-          imgContainer.style.visibility = "visible"
-
-        
-}
-
-
-searchButton.addEventListener("click", async function(event) {
-
-    const pokemonChoice = textField.value
-   console.log(pokemonChoice)
-
-    try {
+    const compPokeone = text.value
+  
+   console.log(compPokeone)
+    
+       try {
              
-        const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonChoice}`)
+        const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${compPokeone}`)
 
          if (!apiResponse.ok) {
             throw new Error("error thrown")
         }
 
         const data =  await apiResponse.json();
+        console.log(data)
+        
+
+         renderPokemonTest(data,stats,pContainer,pImg,compPokeone,nameContainer)
 
         
-        console.log(data)
-        console.log(data.sprites.front_default)
+     
+        
 
-        renderPokemon(data);
+     
 
  
 
@@ -61,21 +54,67 @@ searchButton.addEventListener("click", async function(event) {
         
 
         
-
-        
+  
+          
 
     } catch (error) {
 
         console.error(error.message)
-        pokemonName.innerHTML = "ERROR: you seem to of typed something thats not a pokemon name, please try again."
-        pokeDiv.style.visibility = "visible"
-        pokeStats.style.visibility = "hidden"
-        imgContainer.style.visibility = "hidden"
+       
         
     }
 
-    
+} 
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+if (searchButton) {
+searchButton.addEventListener("click", function () {
+    apiCall(textField,pokeStats,pokeDiv,imgContainer,pokemonName)
 })
+} 
+//--------------------------------------------------------------------------------------
 
 
+let renderPokemonTest = function(data,compStats,rContainer,pokeImg,name,nameContainer) {
 
+    const statData = data.stats;
+
+     
+        
+     compStats.innerHTML = "";
+        for (let i = 0; i < statData.length; i++) {
+            const paragraph = document.createElement("p")
+
+            paragraph.textContent = `${statData[i].stat.name}: ${statData[i].base_stat} `
+
+            compStats.appendChild(paragraph)
+            
+        }
+         pokeImg.src  = data.sprites.front_default
+           nameContainer.innerHTML = name 
+          compStats.style.visibility = "visible"
+         rContainer.style.visibility = "visible"
+          pokeImg.style.visibility = "visible"
+
+        
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+
+    
+
+if(compBtnOne) {
+compBtnOne.addEventListener("click", function () {
+
+    apiCall(compTextOne,compStatsOne,compDivOne,compImgOne)
+})}
+
+if(compBtntwo){
+compBtntwo.addEventListener("click", function() {
+    apiCall(compTexttwo,compStatstwo,compDivtwo,compImgtwo,compNameTwo)
+})}
